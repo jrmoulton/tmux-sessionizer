d004ea8e2dc1ef1ee60204fd0131ded4ba609afd
Marc Jakobi <marc.jakobi@tiko.energy>
2023-12-07 12:08:36 +0100

    build: Add nix flake + .envrc

---
+++ flake.nix
@@ -0,0 +1,66 @@
+{
+  description = "devShell for Rust projects";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs = inputs @ {
+    self,
+    nixpkgs,
+    flake-parts,
+    ...
+  }:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = [
+        "x86_64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      perSystem = {
+        config,
+        self',
+        inputs',
+        pkgs,
+        system,
+        ...
+      }: let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [
+            self.overlays.default
+          ];
+        };
+      in {
+        packages = rec {
+          default = tmux-sessionizer;
+          inherit (pkgs) tmux-sessionizer;
+        };
+        devShells.default = pkgs.mkShell {
+          name = "rust devShell";
+          buildInputs = with pkgs;
+          with pkgs.rustPlatform; [
+            cargo
+            rustc
+            rustfmt
+            rust-analyzer
+            openssl
+            pkg-config
+          ];
+        };
+      };
+      flake = {
+        overlays.default = final: prev: {
+          tmux-sessionizer = prev.tmux-sessionizer.overrideAttrs (oa: {
+            src = self;
+            version = ((final.lib.importTOML "${self}/Cargo.toml").package).version;
+            cargoDeps = final.rustPlatform.importCargoLock {
+              lockFile = self + "/Cargo.lock";
+            };
+          });
+        };
+
+      };
+    };
+}
